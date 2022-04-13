bfd1015c10db1b7a6afbe1e55564cd71bfb39746
dholon akter <d.akter@student.fontys.nl>
2022-04-13 14:07:24 +0200

    new things added

--- Python projects/quiz_game.py
+++ Python projects/quiz_game.py
@@ -3,6 +3,7 @@ playing=input("Do you want to play? ")
 if playing !="yes":
     quit()
 print("Okay!Let's play :)")
+print("I do not know what is happening")
 answer=input("What does CPU stand for?")
 print("Remember, CPU is an abbreviation!")
 print("Remember, it is in your PC!!")
