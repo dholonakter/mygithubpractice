794eef484df4101003751055a4c08f103f75ba1f
dholon akter <d.akter@student.fontys.nl>
2022-04-13 12:57:18 +0200

    commit updated

--- Python projects/quiz_game.py
+++ Python projects/quiz_game.py
@@ -3,3 +3,8 @@ playing=input("Do you want to play? ")
 if playing !="yes":
     quit()
 print("Okay!Let's play :)")
+answer=input("What does CPU stand for?")
+if answer=="Central processing unit":
+    print('Correct!')
+else:
+    print("Incorrect")
