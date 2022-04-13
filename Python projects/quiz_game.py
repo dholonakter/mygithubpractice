25eb97754bf58d69ebd3012c6e89953e79305552
dholon akter <d.akter@student.fontys.nl>
2022-04-13 14:05:38 +0200

    update conflicts

--- Python projects/quiz_game.py
+++ Python projects/quiz_game.py
@@ -3,6 +3,7 @@ playing=input("Do you want to play? ")
 if playing !="yes":
     quit()
 print("Okay!Let's play :)")
+print("Learning merge conflicts")
 answer=input("What does CPU stand for?")
 print("Remember, CPU is an abbreviation!")
 print("Remember, it is in your PC!!")
